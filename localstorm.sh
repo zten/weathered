d7ffdd3378781112a371e2fefea3c744efa2eea0
Christopher Childs <cjchilds@me.com>
2012-06-19 18:55:23 -0400

    newratio

--- localstorm.sh
+++ localstorm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #export MAVEN_OPTS="-server -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:+PrintGC -XX:+PrintGCDetails -Xms128m -Xmx128m"
-export MAVEN_OPTS="-server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+PrintGC -XX:+PrintGCDetails -Xms128m -Xmx128m"
+export MAVEN_OPTS="-server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:NewRatio=3 -Xms128m -Xmx128m"
 mvn compile exec:java -Dexec.classpathScope=compile -Dexec.mainClass=weathered.LoadISDStorm -Dweathered.dataDir=/Users/dubba/Documents/workspace/weathered/testdata -Dlog4j.configuration=log4j.xml
