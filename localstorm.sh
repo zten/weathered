9e38088087d6c57df7e1839e4c9b7d8497fcd32a
Christopher Childs <cjchilds@me.com>
2012-08-11 01:00:19 -0400

    Excising some old Storm-related stuff.

--- localstorm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#export MAVEN_OPTS="-server -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:+PrintGC -XX:+PrintGCDetails -Xms128m -Xmx128m -XX:NewRatio=3"
-export MAVEN_OPTS="-server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+PrintGC -XX:+PrintGCDetails -Xms128m -Xmx128m -XX:NewRatio=3 -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing"
-#export MAVEN_OPTS="-server -XX:+UseG1GC -Xms128m -Xmx128m"
-mvn compile exec:java -Dexec.classpathScope=compile -Dexec.mainClass=weathered.LoadISDStorm -Dlog4j.configuration=log4j.xml -Dexec.args="http://127.0.0.1:8080/010010-99999-2012 127.0.0.1 1 4 2"
